828409b023dcd672ceb125d12b3b95bfeeb910f8
ThalesLoreto <tloreto.dev@gmail.com>
2025-08-16 15:35:48 -0300

    feat(device): add business rules to Device model

--- internal/models/device.go
+++ internal/models/device.go
@@ -1,6 +1,9 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type DeviceState string
 
@@ -10,7 +13,7 @@ const (
 	StateInactive  DeviceState = "inactive"
 )
 
-func (ds DeviceState) isValid() bool {
+func (ds DeviceState) IsValid() bool {
 	switch ds {
 	case StateAvailable, StateInUse, StateInactive:
 		return true
@@ -25,4 +28,57 @@ type Device struct {
 	Brand        string      `json:"brand"`
 	State        DeviceState `json:"state"`
 	CreationTime time.Time   `json:"creation_time"`
+}
+
+func NewDevice(id, name, brand string, state DeviceState) (*Device, error) {
+	if !state.IsValid() {
+		return nil, errors.New("invalid device state")
+	}
+	if id == "" {
+		return nil, errors.New("id cannot be empty")
+	}
+	if name == "" {
+		return nil, errors.New("name cannot be empty")
+	}
+	if brand == "" {
+		return nil, errors.New("brand cannot be empty")
+	}
+	return &Device{
+		ID:           id,
+		Name:         name,
+		Brand:        brand,
+		State:        state,
+		CreationTime: time.Now(),
+	}, nil
+}
+
+func (d *Device) CanUpdateNameAndBrand() bool {
+	return d.State != StateInUse
+}
+
+func (d *Device) CanDelete() bool {
+	return d.State != StateInUse
+}
+
+func (d *Device) UpdateState(newState DeviceState) error {
+	if !newState.IsValid() {
+		return errors.New("invalid device state")
+	}
+	d.State = newState
+	return nil
+}
+
+func (d *Device) UpdateNameAndBrand(newName, newBrand string) error {
+	if !d.CanUpdateNameAndBrand() {
+		return errors.New("cannot update name and brand while device is in use")
+	}
+	if newName == "" {
+		return errors.New("name cannot be empty")
+	}
+	if newBrand == "" {
+		return errors.New("brand cannot be empty")
+	}
+	d.Name = newName
+	d.Brand = newBrand
+	return nil
 }
